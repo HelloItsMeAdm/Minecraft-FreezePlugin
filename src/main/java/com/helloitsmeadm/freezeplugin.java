73486bbc91f0e0f1b047f2541a52afef65c6b653
HelloItsMeAdm <vojtaa29@seznam.cz>
2023-01-29 11:10:05 +0100

    init

---
+++ src/main/java/com/helloitsmeadm/freezeplugin.java
@@ -0,0 +1,17 @@
+package com.helloitsmeadm;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class freezeplugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
